7a381ba1904191600ced3076eb3f837ac89a22af
Kevin <kevin.sm89@gmail.com>
2024-06-16 21:39:02 +0200

    boton de enviar con steps

--- main.js
+++ main.js
@@ -51,40 +51,13 @@ async function boton1() {
     console.log('click en el boton 1', "EN", en, "PULL", pull, "DIR", dir)
     data = `${en},${pull},${dir}\n`;
     console.log('data', data)
-            if (!port) {
-                try {
-                    port = await navigator.serial.requestPort();
-                    await port.open({ baudRate: 9600 });
-          
-                    const encoder = new TextEncoderStream();
-                    writer = encoder.writable.getWriter();
-                    encoder.readable.pipeTo(port.writable);
-          
-                    const decoder = new TextDecoderStream();
-                    reader = decoder.readable.getReader();
-                    port.readable.pipeTo(decoder.writable);
-                    
-                    readSerial()
-                    } catch (error) {
-                        console.error('Error al conectar con el puerto serial: ', error);
-                        }
-                        } if (port && writer) {
-                            try {
-                                await writer.write(data);
-                                console.log('Cadena enviada: ', data);
-                                } catch (error) {
-                                    console.error('Error al enviar la cadena: ', error);
-                                    }
-                                    }
-                //   document.getElementById('output').textContent += value + '\n';
                 
-                let elemento = document.getElementById('boton1');
-                if(en) {
-                    elemento.style.backgroundColor = 'red';
-                }else{
-                    elemento.style.backgroundColor = 'rgba(255, 0, 0, 0.500)';
-                }
-                console.log('elemento', elemento)
+    let elemento = document.getElementById('boton1');
+    if(en) {
+        elemento.style.backgroundColor = 'red';
+    }else{
+        elemento.style.backgroundColor = 'rgba(255, 0, 0, 0.500)';
+    }
 }
 
 async function boton2() {
@@ -94,38 +67,12 @@ async function boton2() {
     data = `${en},${pull},${dir}\n`;
     console.log('data', data)
      
-        if (!port) {
-            try {
-                port = await navigator.serial.requestPort();
-                await port.open({ baudRate: 9600 });
-      
-                const encoder = new TextEncoderStream();
-                writer = encoder.writable.getWriter();
-                encoder.readable.pipeTo(port.writable);
-      
-                const decoder = new TextDecoderStream();
-                reader = decoder.readable.getReader();
-                port.readable.pipeTo(decoder.writable);
-      
-                readSerial()
-            } catch (error) {
-                console.error('Error al conectar con el puerto serial: ', error);
-            }
-    } if (port && writer) {
-try {
-  await writer.write(data);
-  console.log('Cadena enviada: ', data);
-} catch (error) {
-  console.error('Error al enviar la cadena: ', error);
-}
-}
-        let elemento = document.getElementById('boton2');
-        if(dir) {
-            elemento.style.backgroundColor = 'green';
-        }else{
-            elemento.style.backgroundColor = 'rgba(0, 128, 0, 0.500)';
-        }
-        console.log('elemento', elemento)
+    let elemento = document.getElementById('boton2');
+    if(dir) {
+        elemento.style.backgroundColor = 'green';
+    }else{
+        elemento.style.backgroundColor = 'rgba(0, 128, 0, 0.500)';
+    }
     }
 
 async function boton3() {
@@ -134,43 +81,16 @@ async function boton3() {
     data = `${en},${pull},${dir}\n`;
     console.log('data', data)
 
-    if (!port) {
-        try {
-            port = await navigator.serial.requestPort();
-            await port.open({ baudRate: 9600 });
-  
-            const encoder = new TextEncoderStream();
-            writer = encoder.writable.getWriter();
-            encoder.readable.pipeTo(port.writable);
-  
-            const decoder = new TextDecoderStream();
-            reader = decoder.readable.getReader();
-            port.readable.pipeTo(decoder.writable);
-  
-            readSerial()
-        } catch (error) {
-            console.error('Error al conectar con el puerto serial: ', error);
-        }
-    } if (port && writer) {
-    try {
-    await writer.write(data);
-    console.log('Cadena enviada: ', data);
-    } catch (error) {
-    console.error('Error al enviar la cadena: ', error);
-    }
-
     let elemento = document.getElementById('boton3');
     if(pull) {
         elemento.style.backgroundColor = 'blue';
     }else{
         elemento.style.backgroundColor = 'rgba(0, 0, 255, 0.500)';
     }
-    console.log('elemento', elemento)
 
+  
     }
-    
-    
-    }
+
         async function readSerial() {
             while (true) {
             const { value, done } = await reader.read();
@@ -183,3 +103,48 @@ async function boton3() {
             //   document.getElementById('output').textContent += value + '\n';
             }
         }
+
+
+        async function boton4() {
+            console.log('data', data)
+            var stepsNumberInput = document.getElementById('Input').value;
+            console.log('stepsNumberInput', stepsNumberInput)
+            console.log('click en el boton 3', "EN", en, "PULL", pull, "DIR", dir, 'stepsNumberInput', stepsNumberInput)
+            data = `${en},${pull},${dir},${stepsNumberInput}\n`;
+            
+            if (!port) {
+                try {
+                    port = await navigator.serial.requestPort();
+                    await port.open({ baudRate: 9600 });
+          
+                    const encoder = new TextEncoderStream();
+                    writer = encoder.writable.getWriter();
+                    encoder.readable.pipeTo(port.writable);
+          
+                    const decoder = new TextDecoderStream();
+                    reader = decoder.readable.getReader();
+                    port.readable.pipeTo(decoder.writable);
+          
+                    readSerial()
+                } catch (error) {
+                    console.error('Error al conectar con el puerto serial: ', error);
+                }
+            } if (port && writer) {
+            try {
+            await writer.write(data);
+            console.log('Cadena enviada: ', data);
+            } catch (error) {
+            console.error('Error al enviar la cadena: ', error);
+            }
+        
+            let elemento = document.getElementById('boton3');
+            if(pull) {
+                elemento.style.backgroundColor = 'blue';
+            }else{
+                elemento.style.backgroundColor = 'rgba(0, 0, 255, 0.500)';
+            }
+        
+            }
+            }
+
+        
